5750c929de98c27516942f4fb120e38eb274152e
neo <neomn110@gmail.com>
2021-10-26 10:14:46 +0330

    my first commit . database added to the project for first time

---
+++ database/migrations/2021_10_25_074454_create_customer_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastName');
+            $table->string('phoneNumber');
+            $table->string('email');
+            $table->string('purchasedProducts');
+            $table->timestamp('registerDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
