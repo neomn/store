630e558d64df85d4bbbc98be40de1f5589c366ce
neo <neomn110@gmail.com>
2022-01-15 08:45:29 +0330

    master >>> customer migration file deleted and admin migration renamed to admins

--- database/migrations/2022_01_11_044429_create_admins_table.php
+++ database/migrations/2022_01_11_044429_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAdminTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->String('userName');
             $table->String('name');
@@ -32,6 +32,6 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('admins');
     }
 }
