22b8f7ba681a243b6121412709d7868345bffe32
neo <neomn110@gmail.com>
2022-04-01 14:54:36 +0430

    bug fixed in Admin Model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\Admin as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Admin extends Authenticatable
 {
