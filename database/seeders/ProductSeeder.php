4be2ada1b2a4cfa30811790598479df271264605
neo <neomn110@gmail.com>
2022-04-05 07:09:48 +0430

    exception handled while seeding products with its relationships

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -27,21 +27,21 @@ class ProductSeeder extends Seeder
        Product::factory(30)->has(View::factory()->count(random_int(0,20)))
            ->has(Price::factory()->count(random_int(0 , 20)))
            ->has(Sell::factory()->count(random_int(0 , 20)))
-           ->has(Image::factory(random_int(0 , 5)))
+           ->has(Image::factory(random_int(1 , 5)))
            ->for($arduino)
            ->create();
 
         Product::factory(30)->has(View::factory()->count(random_int(0,20)))
             ->has(Price::factory()->count(random_int(0 , 20)))
             ->has(Sell::factory()->count(random_int(0 , 20)))
-            ->has(Image::factory(random_int(0 , 5)))
+            ->has(Image::factory(random_int(1 , 5)))
             ->for($raspberry)
             ->create();
 
         Product::factory(30)->has(View::factory()->count(random_int(0,20)))
             ->has(Price::factory()->count(random_int(0 , 20)))
             ->has(Sell::factory()->count(random_int(0 , 20)))
-            ->has(Image::factory(random_int(0 , 5)))
+            ->has(Image::factory(random_int(1 , 5)))
             ->for($esp)
             ->create();
     }
