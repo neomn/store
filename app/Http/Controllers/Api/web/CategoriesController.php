981bca0a8ef32b186b90868d0d67e2cd59b8786f
neo <neomn110@gmail.com>
2022-04-22 12:30:35 +0430

    category products with thier latest price can be queried using api , but not formatted yet , have to create api resorce for them

--- app/Http/Controllers/Api/web/CategoriesController.php
+++ app/Http/Controllers/Api/web/CategoriesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api\web;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\CategoryResource;
 use App\Models\Category;
+use App\Models\Price;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 
@@ -17,11 +19,14 @@ class CategoriesController extends Controller
     public function retrieveCategoryProducts($category){
         $category = Category::where('category' , $category)->first();
         $products = $category->products;
-//        $productsLatestPrice = [];
-//        foreach ($products as $product){
-//
-//        }
-        return response($products);
+        $productsLatestPrice = [];
+        foreach ($products as $product){
+            $productId = $product->id;
+            $price = $product->latestPrice->price;
+            $productPrice = [$productId , $price];
+            $productsLatestPrice [] = $productPrice;
+        }
+        return response([$products , $productsLatestPrice]);
     }
 
     public function show($id){
