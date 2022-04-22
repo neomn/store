44ae68c99853ccb69ad46c896e15ea7e051ac3e6
neo <neomn110@gmail.com>
2022-04-22 07:18:38 +0430

    category resource created and defined to format the category api , its ok but category ui section has stopped working

--- app/Http/Controllers/Api/web/CategoriesController.php
+++ app/Http/Controllers/Api/web/CategoriesController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api\web;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CategoryResource;
 use App\Models\Category;
 
 class CategoriesController extends Controller
 {
     public function index(){
 //        return response('allCategories index');
-        return response(Category::all());
+//        return response(Category::all());
+        return CategoryResource::collection(Category::all());
     }
 }
