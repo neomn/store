e33f8fbade96155a532c8f9b7e64b36acf713d50
neo <neomn110@gmail.com>
2022-04-22 16:09:31 +0430

    category products function now get the category id  insted of category itself , done with the api , going for frontend

--- app/Http/Controllers/Api/web/CategoriesController.php
+++ app/Http/Controllers/Api/web/CategoriesController.php
@@ -6,10 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\web\CategoryProductsWithPriceResource;
 use App\Http\Resources\Api\web\CategoryResource;
 use App\Models\Category;
-use App\Models\Price;
 use App\Models\Product;
-use Illuminate\Http\Request;
-use phpDocumentor\Reflection\Types\Object_;
+
 
 
 class CategoriesController extends Controller
@@ -18,9 +16,7 @@ class CategoriesController extends Controller
         return CategoryResource::collection(Category::all());
     }
 
-    public function retrieveCategoryProducts($category){
-
-        $categoryId = Category::where('category' , $category)->first()->id;
+    public function retrieveCategoryProducts($categoryId){
         $products = Product::with('latestPrice')->where('category_id' , $categoryId)->get();
         return CategoryProductsWithPriceResource::collection($products);
     }
