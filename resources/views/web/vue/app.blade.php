17876aef194381e74ffe6d2d240503093c93ff34
neo <neomn110@gmail.com>
2022-05-02 13:50:25 +0430

    fix: fix white screen on reload pagees

--- resources/views/web/vue/app.blade.php
+++ resources/views/web/vue/app.blade.php
@@ -13,8 +13,8 @@
 
     <title>Document</title>
 </head>
-<body>
-    <div id="app">
+<body class="bg-slate-800">
+    <div id="app" >
         <App></App>
     </div>
 <script src={{mix("/js/web/web.js")}}></script>
