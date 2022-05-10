624d71bdaed179f307a02cec105ca326224e1214
neo <neomn110@gmail.com>
2022-05-10 15:54:34 +0430

    feat: admin authentication works using cookies but need modification

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\admin\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/panel/{any?}','admin.vue.app')
-    ->middleware('adminAuthenticator')
+//    ->middleware('adminAuthenticator')
     ->where('any','.*?')
     ->name('panel');
