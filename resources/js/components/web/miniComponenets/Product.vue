b9a13c84b780e5b4ce5e3eb831f3cc55c4af604a
neo <neomn110@gmail.com>
2022-05-16 12:08:18 +0430

    feat: create product component

---
+++ resources/js/components/web/miniComponenets/Product.vue
@@ -0,0 +1,35 @@
+<template class="relative">
+    <div class=" absolute top-1/3 left-1/3  w-32 h-52 bg-slate-700 text-zinc-200">
+        <router-link :to="{name: 'product' , params:{product_number: product.number}}">
+            <!-- image -->
+            <div class=" ">
+                <img :src="product.image" alt="132" class="">
+            </div>
+            <!--content -->
+            <div class=" text-sm text-center ">
+                <p>{{ product.name }}</p>
+            </div>
+            <!--buttons -->
+            <div class="">
+
+            </div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Product",
+    props: [
+        'product'
+    ],
+    data() {
+        return {}
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
