33a54de9b3e5e8ab8c917b810bb5e8391d70587a
neo <neomn110@gmail.com>
2022-05-21 06:36:15 +0430

    feat: create admin category api resource

--- app/Http/Controllers/Api/admin/CategoryController.php
+++ app/Http/Controllers/Api/admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -14,7 +15,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return response('this is category controller index ');
+//        return response('this is category controller index ');
+        Category::all()
     }
 
     /**
