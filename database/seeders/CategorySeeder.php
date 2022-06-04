038f675ec1b01485703f9399cca412fc22764c27
neomn110 <neomn110@gmail.com>
2022-06-04 16:02:11 +0430

    style: reduce category seeder codes spacing

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,36 +16,12 @@ class CategorySeeder extends Seeder
     public function run()
     {
         $categories = [
-            [
-                'id' => 1 ,
-                'category' => 'Electronics And Robotics',
-                'parent_id' => null,
-            ],
-            [
-                'id'=> 2,
-                'category' => 'Other',
-                'parent_id' => null ,
-            ],
-            [
-                'id' => 11,
-                'category' => 'MicroControllers and Processors',
-                'parent_id' => 1
-            ],
-            [
-                'id' => 111,
-                'category' => 'Arduino Family',
-                'parent_id' => 11
-            ],
-            [
-                'id' => 112,
-                'category' => 'Raspberry Family',
-                'parent_id' => 11
-            ],
-            [
-                'id' => 113,
-                'category' => 'ESP Family',
-                'parent_id' => 11
-            ],
+            ['id' => 1 , 'category' => 'Electronics And Robotics', 'parent_id' => null,],
+            ['id'=> 2, 'category' => 'Other', 'parent_id' => null ,],
+            ['id' => 11, 'category' => 'MicroControllers and Processors', 'parent_id' => 1],
+            ['id' => 111, 'category' => 'Arduino Family', 'parent_id' => 11],
+            ['id' => 112, 'category' => 'Raspberry Family', 'parent_id' => 11],
+            ['id' => 113, 'category' => 'ESP Family', 'parent_id' => 11],
         ];
         DB::table('categories')->insert($categories);
     }
