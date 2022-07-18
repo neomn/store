40d506bfeeac4da4290030585a4b7522602c1988
neomn110 <neomn110@gmail.com>
2022-07-18 21:27:05 +0430

    feat: validate register request

--- app/Http/Controllers/ExternalApi/AuthController.php
+++ app/Http/Controllers/ExternalApi/AuthController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers\ExternalApi;
 
 use App\Http\Requests\RegisterUserRequest;
 use App\Models\User;
-use Doctrine\DBAL\Query\QueryException;
-use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use mysql_xdevapi\Exception;
 
 class AuthController extends Controller
 {
@@ -18,11 +15,7 @@ class AuthController extends Controller
             'phone_number' => $request->phone_number,
             'password' => Hash::make($request->password),
         ];
-            if ($user = User::create($credentials))
-                return response()->json(['successfully registered' => $user] , '200');
-
-            return response()->json(['error' => 'failed to register user']);
-
+        return response()->json(['successfully registered' => User::create($credentials)] , '200');
     }
 
     public function login(Request $request)
--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -2,30 +2,34 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterUserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-//            'phone_number' => '',
-//            'password' => '',
+            'phone_number' => 'required|digits:11|numeric|unique:users',
+            'password' => 'required|min:8|max:16|',
+        ];
+    }
+    public function messages()
+    {
+        return [
+          'phone_number.required' => 'شماره تلفن الزامی می باشد',
+          'phone_number.digits' => 'شماره تلفن باید ۱۱ رقم می باشد',
+          'phone_number.numeric' => 'شماره تلفن باید فقط شامل عدد باشد',
+          'phone_number.unique' => 'این شماره تلفن قبلا ثبت شده است',
+
+          'password.required' => 'رمز عبور الزامی می باشد',
+          'password.min' => 'رمز عبور باید حد اقل هشت کاراکتر باشد ',
+          'password.max' => 'رمز عبور باید حد اکثر شانزده کاراکتر باشد',
         ];
     }
 }
