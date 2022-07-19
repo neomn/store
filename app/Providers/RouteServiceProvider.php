83609941c361f8e0d9bbe09115ec49a2ab83c7a1
neomn110 <neomn110@gmail.com>
2022-07-19 14:17:39 +0430

    refactor: remove extra route files

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,8 +46,7 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
-
+                ->group(base_path('routes/redirectToSPA.php'));
         });
     }
 
--- routes/redirectToSPA.php
+++ routes/redirectToSPA.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::view('admin/{any}' , 'adminPanel.vue.app')->where('any' , '.*');
 Route::view('/{any}' , 'web.vue.app')->where('any' , '.*');
 
-require __DIR__ . '/auth.php';
